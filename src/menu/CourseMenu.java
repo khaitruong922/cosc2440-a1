1cd087f3c9a63a03b9e9d3677a730941a56f7eee
Khai Truong <kd.kidr@gmail.com>
2021-03-22 18:06:10 +0700

    View courses in semester

--- src/menu/CourseMenu.java
+++ src/menu/CourseMenu.java
@@ -1,5 +1,6 @@
 package menu;
 
+import menu.model.InputField;
 import menu.model.Menu;
 import menu.model.Option;
 import menu.model.Table;
@@ -9,9 +10,11 @@ import repository.InMemoryStudentEnrolmentManager;
 import repository.StudentEnrolmentManager;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CourseMenu extends Menu {
     private final StudentEnrolmentManager sem = new InMemoryStudentEnrolmentManager();
+    private final InputField semesterInput;
 
     public CourseMenu() {
         optionMenu.add(new Option("View courses", "1", () -> {
@@ -19,13 +22,31 @@ public class CourseMenu extends Menu {
             waitForEnter();
             run();
         }));
+        optionMenu.add(new Option("View courses in semester", "2", () -> {
+            viewCoursesInSemester();
+            waitForEnter();
+            run();
+        }));
         optionMenu.add(new Option("Back", "4", () -> {
 
         }));
+        semesterInput = new InputField("Semester: ").required();
+    }
+
+    private void viewCoursesInSemester() {
+        String semester = semesterInput.getInput();
+        List<Enrolment> enrolments = sem.getEnrolments();
+        List<Course> coursesInSemester = enrolments.stream().filter(e -> e.getSemester().equals(semester)).map(Enrolment::getCourse).collect(Collectors.toList());
+        viewCoursesFromList(coursesInSemester);
+
     }
 
     private void viewCourses() {
         List<Course> courses = sem.getCourses();
+        viewCoursesFromList(courses);
+    }
+
+    private void viewCoursesFromList(List<Course> courses) {
         Table table = new Table(Course.getFields());
         courses.forEach(c -> {
             table.addRow(c.toRecord());
